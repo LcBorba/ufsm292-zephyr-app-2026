5f019be2167757176a1007ad92ff22656689de41
Carlos Henrique Barriquello <133696168+ufsm-barriquello@users.noreply.github.com>
2026-09-11 11:21:10 -0300

    Update workspace paths in onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -7,14 +7,14 @@ export PATH="$HOME/.local/bin:$PATH"
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
 pip3 install --user west
 
-cd /workspace
+cd /workspaces
 
 # Trata o repositório montado (que já contém west.yml na raiz) como o
 # repositório-manifesto local do workspace west.
 west init -l ufsm292-zephyr-app-2026
 
 # Baixa o kernel Zephyr e os módulos (HALs SAM0, CMSIS, etc.) declarados
-# no west.yml como pastas irmãs em /workspace (zephyr/, modules/, ...).
+# no west.yml como pastas irmãs em /workspaces (zephyr/, modules/, ...).
 west update
 
 west zephyr-export
@@ -23,7 +23,7 @@ pip3 install --user -r zephyr/scripts/requirements.txt
 
 # Conveniências de terminal
 echo "alias ll='ls -lah'" >> "$HOME/.bashrc"
-echo "cd /workspace/ufsm292-zephyr-app-2026" >> "$HOME/.bashrc"
+echo "cd /workspaces/ufsm292-zephyr-app-2026" >> "$HOME/.bashrc"
 west completion bash > "$HOME/west-completion.bash"
 echo 'source $HOME/west-completion.bash' >> "$HOME/.bashrc"
 
